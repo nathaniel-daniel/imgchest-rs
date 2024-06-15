b2f4f8b8198909361d1d7c3c9943f33f9542cfee
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-06-15 04:49:55 +0000

    deploy: e80bcf7647e8901e40b641e4f83ce65e20d61c7d

---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"imgchest":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"imgchest/enum.PostPrivacy.html\" title=\"enum imgchest::PostPrivacy\">Privacy</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
