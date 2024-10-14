f13a2eaea84e49a836521ab68303b69c5b318b03
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-10-14 19:28:56 +0000

    deploy: 4e7568ddf3097534d734e374f7edefe09d1cfa0e

---
+++ imgchest/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error","InvalidScrapedPostError","InvalidScrapedPostFileError","PostPrivacy"],"struct":["Body","Client","CreatePostBuilder","FileUpdate","Post","PostFile","ScrapedPost","ScrapedPostFile","UpdatePostBuilder","UploadPostFile","User"]};
---
+++ trait.impl/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"imgchest":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"imgchest/enum.PostPrivacy.html\" title=\"enum imgchest::PostPrivacy\">Privacy</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
