600f611bd34d1d88e5a21542a69e8f25f7715f2d
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-04-13 03:40:46 +0000

    deploy: e92394b47dc9908c0b833c61a557c1117b635520

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["imgchest",["",[["client",[],["builder.rs"]],["model",[],["list_posts.rs","post.rs","scraped_post.rs","scraped_user.rs","user.rs"]]],["client.rs","lib.rs","model.rs","serde.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[180]}
